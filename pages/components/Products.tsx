076b9f10e1d98b39fef744d09624571745a85348
Sergio Guerra <sergioguerra1991@gmail.com>
2022-12-29 06:59:41 -0600

    Products data

--- pages/components/Products.tsx
+++ pages/components/Products.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import { FC, useEffect, useMemo, useState } from "react";
-import { Rating } from "react-simple-star-rating";
+import ReactStars from "react-rating-stars-component";
 import products from "../data/products.json"
 
 type ProductItemProps = {
@@ -10,27 +10,26 @@ type ProductItemProps = {
     price: string
     currency: string
     link: string
-    type: string
     wagon?: boolean
 }
 
 // TODO: Wagon: <button className=" bg-white hover:bg-sky-700 hover:text-white  border-2 border-sky-700 text-sky-700 p-1 rounded-lg">Add to wagon</button>
 
 const ProductItem: FC<ProductItemProps> = ({title, image, stars, price, currency, link, wagon=false})=>{
-    const ellipsisTitle = `${title.slice(0,35)} ${title.slice(35)?'...':''}`
+    const ellipsisTitle = `${title.slice(0,50)} ${title.slice(50)?'...':''}`
 
     const handleBuyInShop = ()=>{
         (new Audio('/buy_sound.mp3')).play()
     }
 
     return (
-        <article className="flex flex-col text-center border-2 rounded-lg p-4 h-80 justify-between">
-            <div className="flex w-full justify-center">
+        <article className="flex flex-col text-center border-2 rounded-lg p-4 h-96 justify-between">
+            <div className="flex w-full justify-center max-h-80 overflow-hidden">
              <Image src={image} alt="image" width={100} height={20}/>
             </div>
             <h5 className="text-lg" style={{ fontFamily: 'Zilla Slab Highlight' }}>{ellipsisTitle}</h5>
             <strong className="text-lg">{price} {currency}</strong>
-            <Rating initialValue={stars} readonly size={20} fillClassName="!flex flex-row" emptyClassName="!flex flex-row" tooltipDefaultText={`${stars}`}/>
+            <ReactStars classNames="mx-5" count={5} value={stars} edit={false} size={40} isHalf={true} activeColor="#FF9900"/>
             <a target="_new" href={link} className="group bg-white hover:bg-slate-800 hover:text-amber-500 border-2 border-slate-800 text-slate-800 p-1 rounded-lg cursor-pointer flex" onClick={handleBuyInShop}>
                 <Image src="/product_buy.png" width={25} height={20} alt="Buy icon" className="group-hover:invert mx-2"/>
                 <strong className="ml-2">Buy in shop</strong>
